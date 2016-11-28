df732b25bf748c58f01622d288834cd9a56d2b4e
Sindre Sorhus <sindresorhus@gmail.com>
2016-11-28 12:22:22 +0700

    Rewrite module to preserve order by default (#1)

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import test from 'ava';
+import {serial as test} from 'ava';
 import delay from 'delay';
 import inRange from 'in-range';
 import timeSpan from 'time-span';
@@ -15,14 +15,20 @@ const tester = ([val, ms]) => delay(ms).then(() => val === 2 || val === 3);
 
 test('main', async t => {
 	const end = timeSpan();
-	t.is((await m(input, tester))[0], 3);
-	t.true(inRange(end(), 170, 240), 'should be time of item `3`, since they run concurrently');
+	t.is((await m(input, tester))[0], 2);
+	t.true(inRange(end(), 370, 450), 'should be time of item `2`');
 });
 
-test('matches in order when concurrency:1', async t => {
+test('option {preserveOrder:false}', async t => {
+	const end = timeSpan();
+	t.is((await m(input, tester, {preserveOrder: false}))[0], 3);
+	t.true(inRange(end(), 170, 250), 'should be time of item `3`');
+});
+
+test('option {concurrency:1}', async t => {
 	const end = timeSpan();
 	t.is((await m(input, tester, {concurrency: 1}))[0], 2);
-	t.true(inRange(end(), 670, 740), 'should be time of items `1` and `2`, since they run serially');
+	t.true(inRange(end(), 670, 750), 'should be time of items `1` and `2`, since they run serially');
 });
 
 test('returns `undefined` when nothing could be found', async t => {
