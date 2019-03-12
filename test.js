4466fbe39ceffd47b98d001db8573c6302b15c7a
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-12 09:41:36 +0000

    Require Node.js 8, add TypeScript definition (#3)

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import {serial as test} from 'ava';
 import delay from 'delay';
 import inRange from 'in-range';
 import timeSpan from 'time-span';
-import m from '.';
+import pLocate from '.';
 
 const input = [
 	[1, 300],
@@ -18,27 +18,27 @@ const tester = async ([value, ms]) => {
 
 test('main', async t => {
 	const end = timeSpan();
-	t.is((await m(input, tester))[0], 2);
+	t.is((await pLocate(input, tester))[0], 2);
 	t.true(inRange(end(), 370, 450), 'should be time of item `2`');
 });
 
 test('option {preserveOrder:false}', async t => {
 	const end = timeSpan();
-	t.is((await m(input, tester, {preserveOrder: false}))[0], 3);
+	t.is((await pLocate(input, tester, {preserveOrder: false}))[0], 3);
 	t.true(inRange(end(), 170, 250), 'should be time of item `3`');
 });
 
 test('option {concurrency:1}', async t => {
 	const end = timeSpan();
-	t.is((await m(input, tester, {concurrency: 1}))[0], 2);
+	t.is((await pLocate(input, tester, {concurrency: 1}))[0], 2);
 	t.true(inRange(end(), 670, 750), 'should be time of items `1` and `2`, since they run serially');
 });
 
 test('returns `undefined` when nothing could be found', async t => {
-	t.is((await m([1, 2, 3], () => false)), undefined);
+	t.is((await pLocate([1, 2, 3], () => false)), undefined);
 });
 
 test('rejected return value in `tester` rejects the promise', async t => {
-	const fixtureErr = new Error('fixture');
-	await t.throws(m([1, 2, 3], () => Promise.reject(fixtureErr)), fixtureErr.message);
+	const fixtureError = new Error('fixture');
+	await t.throwsAsync(pLocate([1, 2, 3], () => Promise.reject(fixtureError)), fixtureError.message);
 });
